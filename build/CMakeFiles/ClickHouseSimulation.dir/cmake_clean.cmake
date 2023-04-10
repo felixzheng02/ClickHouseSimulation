09e44e71af91f5483a9e9f044bfadece6e6239e7
Yuchen Zheng <felix02@email.unc.edu>
2023-04-09 23:31:35 -0400

    Upload build.

---
+++ build/CMakeFiles/ClickHouseSimulation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClickHouseSimulation.dir/src/main.cpp.o"
+  "CMakeFiles/ClickHouseSimulation.dir/src/main.cpp.o.d"
+  "ClickHouseSimulation"
+  "ClickHouseSimulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClickHouseSimulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
